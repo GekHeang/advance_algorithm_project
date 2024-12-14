5c32f48131da1721c428a67d7c04a47b8fd0d66f
Gek Heang <muonggekheang05@gmail.com>
2024-12-14 17:17:31 +0700

    created a constructor and 2 functions in header.h file and tested it on project_main.cpp file

--- header.h
+++ header.h
@@ -1,6 +1,5 @@
 #include<iostream>
 
-
 using namespace std;
 struct Node{
     string name;
@@ -15,10 +14,88 @@ struct Node{
     string date_of_birth;
     long int national_id;
     Node *next;
+    string status;
 };
 
 class employees_data_structure
 {
     public:
-    
+    int size;
+    Node *front;
+    Node *rear;
+
+    employees_data_structure(){
+        size = 0;
+        front = NULL;
+        rear = NULL;
+    }
+
+    void add_new_employee_info(string name, int id, string position, string hire_date, string gender, int age, float salary, string contact_info, string address, string date_of_birth, long int national_id, string status){
+        Node *new_node;
+        new_node = new Node;
+        new_node->name = name;
+        new_node->id = id;
+        new_node->position = position;
+        new_node->hire_date = hire_date;
+        new_node->gender = gender;
+        new_node->age = age;
+        new_node->salary = salary;
+        new_node->contact_info = contact_info;
+        new_node->address = address;
+        new_node->date_of_birth = date_of_birth;
+        new_node->national_id = national_id;
+        new_node->status = status;
+        new_node->next = NULL;
+        if (front == NULL)
+        {
+            front = new_node;
+            rear = new_node;
+        }
+        else{
+            rear->next = new_node;
+            rear = new_node;
+        }
+    }
+
+    void delete_employee_info(){
+        if (size == 0)
+        {
+            cout << "Data structure is empty. cannot delete" << endl;
+        }
+        else{
+            Node *temp;
+            temp = front;
+            front = front->next;
+            delete temp;
+            size--;
+        }
+        
+    }
+
+    void display(){
+        Node *temp;
+        temp = front;
+        while (front != NULL)
+        {
+            cout << temp->name << "," 
+                << temp->id << ","
+                << temp->position << ","
+                << temp->hire_date << ","
+                << temp->gender << ","
+                << temp->age << ","
+                << temp->salary << ","
+                << temp->contact_info << ","
+                << temp->address << ","
+                << temp->address << ","
+                << temp->date_of_birth << ","
+                << temp->national_id << ","
+                << temp->status << ","
+                <<endl;
+            temp = temp->next;
+        }
+
+        
+        
+    }
+
 };
--- project_main.cpp
+++ project_main.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
-
+#include<header.h>
 using namespace std;
 int main(){
+    employees_data_structure *employee_1 = new employees_data_structure;
+    employee_1->add_new_employee_info("Sarah Johnson", 10123, "Software Engineer", "2020-08-15", "Female", 29, 75000.50, "+1-555-789-1234", "123 Elm Street, Springfield, IL, USA", "1995-04-22", 9876543210123, "Active"
+);
+    employee_1->display();
     return 0;
 }
