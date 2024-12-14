86bf2c2ac39b210387e782cc7c9453ddca0a4893
Gek Heang <muonggekheang05@gmail.com>
2024-12-14 16:25:17 +0700

    created header.h file and added struct

---
+++ header.h
@@ -0,0 +1,24 @@
+#include<iostream>
+
+
+using namespace std;
+struct Node{
+    string name;
+    int id;
+    string position;
+    string hire_date;
+    string gender;
+    int age;
+    float salary;
+    string contact_info;
+    string address;
+    string date_of_birth;
+    long int national_id;
+    Node *next;
+};
+
+class employees_data_structure
+{
+    public:
+    
+};
